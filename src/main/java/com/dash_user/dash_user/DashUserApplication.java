968b482fbe294c793a559fd17861510af311881d
Robson Neves <robsonnevesrodrigues@yahoo.com.br>
2020-09-30 15:28:03 -0300

    Primeiro commit

---
+++ src/main/java/com/dash_user/dash_user/DashUserApplication.java
@@ -0,0 +1,13 @@
+package com.dash_user.dash_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dash_user/dash_user/DashUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dash_user.dash_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
